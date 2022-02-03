9baa6bf7e0d0bddc7cccb5c47cc745f1494660f7
ejllovido <ericllovido08@gmail.com>
2022-02-03 20:03:17 +0800

    Refactors js

--- index.js
+++ index.js
@@ -31,158 +31,128 @@ async function getData() {
   displayDaily()
 }
 
+function addAnimationNewHrs(element) {
+  return element.classList.add('hrs', 'new-hrs')
+}
+
+function removeAnimationNewHrs(element) {
+  return setTimeout(() => {
+    element.classList.remove('new-hrs')
+  }, 400)
+}
+
+function updateCurrent(element, dataFetch, index, date) {
+  addAnimationNewHrs(element)
+  removeAnimationNewHrs(element)
+  return (element.innerHTML = dataFetch[index].timeframes[date].current + 'hrs')
+}
+
+function updatePrevious(element, dataFetch, index, date) {
+  return (element.innerHTML = dataFetch[index].timeframes[date].previous + 'hrs')
+}
+
+function updateButtonColor(element, color) {
+  return (element.style.color = color)
+}
+
 function displayDaily() {
   for (let i = 0; i < dataFetch.length; i++) {
-    if (dataFetch[i].title === 'Work') {
-      currentWork.classList.add('hrs', 'new-hrs')
-      currentWork.innerHTML = dataFetch[i].timeframes.daily.current + 'hrs'
-      setTimeout(() => {
-        currentWork.classList.remove('new-hrs')
-      }, 400)
-      prevWork.innerHTML = dataFetch[i].timeframes.daily.previous + 'hrs'
-    } else if (dataFetch[i].title === 'Play') {
-      currentPlay.classList.add('hrs', 'new-hrs')
-      currentPlay.innerHTML = dataFetch[i].timeframes.daily.current + 'hrs'
-      setTimeout(() => {
-        currentPlay.classList.remove('new-hrs')
-      }, 400)
-      prevPlay.innerHTML = dataFetch[i].timeframes.daily.previous + 'hrs'
-    } else if (dataFetch[i].title === 'Study') {
-      currentStudy.classList.add('hrs', 'new-hrs')
-      currentStudy.innerHTML = dataFetch[i].timeframes.daily.current + 'hrs'
-      setTimeout(() => {
-        currentStudy.classList.remove('new-hrs')
-      }, 400)
-      prevStudy.innerHTML = dataFetch[i].timeframes.daily.previous + 'hrs'
-    } else if (dataFetch[i].title === 'Exercise') {
-      currentExercise.classList.add('hrs', 'new-hrs')
-      currentExercise.innerHTML = dataFetch[i].timeframes.daily.current + 'hrs'
-      setTimeout(() => {
-        currentExercise.classList.remove('new-hrs')
-      }, 400)
-      prevExercise.innerHTML = dataFetch[i].timeframes.daily.previous + 'hrs'
-    } else if (dataFetch[i].title === 'Social') {
-      currentSocial.classList.add('hrs', 'new-hrs')
-      currentSocial.innerHTML = dataFetch[i].timeframes.daily.current + 'hrs'
-      setTimeout(() => {
-        currentSocial.classList.remove('new-hrs')
-      }, 400)
-      prevSocial.innerHTML = dataFetch[i].timeframes.daily.previous + 'hrs'
-    } else if (dataFetch[i].title === 'Self Care') {
-      currentCare.classList.add('hrs', 'new-hrs')
-      currentCare.innerHTML = dataFetch[i].timeframes.daily.current + 'hrs'
-      setTimeout(() => {
-        currentCare.classList.remove('new-hrs')
-      }, 400)
-      prevCare.innerHTML = dataFetch[i].timeframes.daily.previous + 'hrs'
+    switch (dataFetch[i].title) {
+      case 'Work':
+        updateCurrent(currentWork, dataFetch, i, 'daily')
+        updatePrevious(prevWork, dataFetch, i, 'daily')
+        break
+      case 'Play':
+        updateCurrent(currentPlay, dataFetch, i, 'daily')
+        updatePrevious(prevPlay, dataFetch, i, 'daily')
+        break
+      case 'Study':
+        updateCurrent(currentStudy, dataFetch, i, 'daily')
+        updatePrevious(prevStudy, dataFetch, i, 'daily')
+        break
+      case 'Exercise':
+        updateCurrent(currentExercise, dataFetch, i, 'daily')
+        updatePrevious(prevExercise, dataFetch, i, 'daily')
+        break
+      case 'Social':
+        updateCurrent(currentSocial, dataFetch, i, 'daily')
+        updatePrevious(prevSocial, dataFetch, i, 'daily')
+        break
+      default:
+        updateCurrent(currentCare, dataFetch, i, 'daily')
+        updatePrevious(prevCare, dataFetch, i, 'daily')
     }
   }
 
-  navDaily.style.color = '#fff'
-  navWeekly.style.color = '#6f76c8'
-  navMonthly.style.color = '#6f76c8'
+  updateButtonColor(navDaily, '#fff')
+  updateButtonColor(navWeekly, '#6f76c8')
+  updateButtonColor(navMonthly, '#6f76c8')
 }
 
 function displayWeekly() {
   for (let i = 0; i < dataFetch.length; i++) {
-    if (dataFetch[i].title === 'Work') {
-      currentWork.classList.add('hrs', 'new-hrs')
-      currentWork.innerHTML = dataFetch[i].timeframes.weekly.current + 'hrs'
-      setTimeout(() => {
-        currentWork.classList.remove('new-hrs')
-      }, 400)
-      prevWork.innerHTML = dataFetch[i].timeframes.weekly.previous + 'hrs'
-    } else if (dataFetch[i].title === 'Play') {
-      currentPlay.classList.add('hrs', 'new-hrs')
-      currentPlay.innerHTML = dataFetch[i].timeframes.weekly.current + 'hrs'
-      setTimeout(() => {
-        currentPlay.classList.remove('new-hrs')
-      }, 400)
-      prevPlay.innerHTML = dataFetch[i].timeframes.weekly.previous + 'hrs'
-    } else if (dataFetch[i].title === 'Study') {
-      currentStudy.classList.add('hrs', 'new-hrs')
-      currentStudy.innerHTML = dataFetch[i].timeframes.weekly.current + 'hrs'
-      setTimeout(() => {
-        currentStudy.classList.remove('new-hrs')
-      }, 400)
-      prevStudy.innerHTML = dataFetch[i].timeframes.weekly.previous + 'hrs'
-    } else if (dataFetch[i].title === 'Exercise') {
-      currentExercise.classList.add('hrs', 'new-hrs')
-      currentExercise.innerHTML = dataFetch[i].timeframes.weekly.current + 'hrs'
-      setTimeout(() => {
-        currentExercise.classList.remove('new-hrs')
-      }, 400)
-      prevExercise.innerHTML = dataFetch[i].timeframes.weekly.previous + 'hrs'
-    } else if (dataFetch[i].title === 'Social') {
-      currentSocial.classList.add('hrs', 'new-hrs')
-      currentSocial.innerHTML = dataFetch[i].timeframes.weekly.current + 'hrs'
-      setTimeout(() => {
-        currentSocial.classList.remove('new-hrs')
-      }, 400)
-      prevSocial.innerHTML = dataFetch[i].timeframes.weekly.previous + 'hrs'
-    } else if (dataFetch[i].title === 'Self Care') {
-      currentCare.classList.add('hrs', 'new-hrs')
-      currentCare.innerHTML = dataFetch[i].timeframes.weekly.current + 'hrs'
-      setTimeout(() => {
-        currentCare.classList.remove('new-hrs')
-      }, 400)
-      prevCare.innerHTML = dataFetch[i].timeframes.weekly.previous + 'hrs'
+    switch (dataFetch[i].title) {
+      case 'Work':
+        updateCurrent(currentWork, dataFetch, i, 'weekly')
+        updatePrevious(prevWork, dataFetch, i, 'weekly')
+        break
+      case 'Play':
+        updateCurrent(currentPlay, dataFetch, i, 'weekly')
+        updatePrevious(prevPlay, dataFetch, i, 'weekly')
+        break
+      case 'Study':
+        updateCurrent(currentStudy, dataFetch, i, 'weekly')
+        updatePrevious(prevStudy, dataFetch, i, 'weekly')
+        break
+      case 'Exercise':
+        updateCurrent(currentExercise, dataFetch, i, 'weekly')
+        updatePrevious(prevExercise, dataFetch, i, 'weekly')
+        break
+      case 'Social':
+        updateCurrent(currentSocial, dataFetch, i, 'weekly')
+        updatePrevious(prevSocial, dataFetch, i, 'weekly')
+        break
+      default:
+        updateCurrent(currentCare, dataFetch, i, 'weekly')
+        updatePrevious(prevCare, dataFetch, i, 'weekly')
     }
   }
 
-  navDaily.style.color = '#6f76c8'
-  navWeekly.style.color = '#fff'
-  navMonthly.style.color = '#6f76c8'
+  updateButtonColor(navDaily, '#6f76c8')
+  updateButtonColor(navWeekly, '#fff')
+  updateButtonColor(navMonthly, '#6f76c8')
 }
 
 function displayMonthly() {
   for (let i = 0; i < dataFetch.length; i++) {
-    if (dataFetch[i].title === 'Work') {
-      currentWork.classList.add('hrs', 'new-hrs')
-      currentWork.innerHTML = dataFetch[i].timeframes.monthly.current + 'hrs'
-      setTimeout(() => {
-        currentWork.classList.remove('new-hrs')
-      }, 400)
-      prevWork.innerHTML = dataFetch[i].timeframes.monthly.previous + 'hrs'
-    } else if (dataFetch[i].title === 'Play') {
-      currentPlay.classList.add('hrs', 'new-hrs')
-      currentPlay.innerHTML = dataFetch[i].timeframes.monthly.current + 'hrs'
-      setTimeout(() => {
-        currentPlay.classList.remove('new-hrs')
-      }, 400)
-      prevPlay.innerHTML = dataFetch[i].timeframes.monthly.previous + 'hrs'
-    } else if (dataFetch[i].title === 'Study') {
-      currentStudy.classList.add('hrs', 'new-hrs')
-      currentStudy.innerHTML = dataFetch[i].timeframes.monthly.current + 'hrs'
-      setTimeout(() => {
-        currentStudy.classList.remove('new-hrs')
-      }, 400)
-      prevStudy.innerHTML = dataFetch[i].timeframes.monthly.previous + 'hrs'
-    } else if (dataFetch[i].title === 'Exercise') {
-      currentExercise.classList.add('hrs', 'new-hrs')
-      currentExercise.innerHTML = dataFetch[i].timeframes.monthly.current + 'hrs'
-      setTimeout(() => {
-        currentExercise.classList.remove('new-hrs')
-      }, 400)
-      prevExercise.innerHTML = dataFetch[i].timeframes.monthly.previous + 'hrs'
-    } else if (dataFetch[i].title === 'Social') {
-      currentSocial.classList.add('hrs', 'new-hrs')
-      currentSocial.innerHTML = dataFetch[i].timeframes.monthly.current + 'hrs'
-      setTimeout(() => {
-        currentSocial.classList.remove('new-hrs')
-      }, 400)
-      prevSocial.innerHTML = dataFetch[i].timeframes.monthly.previous + 'hrs'
-    } else if (dataFetch[i].title === 'Self Care') {
-      currentCare.classList.add('hrs', 'new-hrs')
-      currentCare.innerHTML = dataFetch[i].timeframes.monthly.current + 'hrs'
-      setTimeout(() => {
-        currentCare.classList.remove('new-hrs')
-      }, 400)
-      prevCare.innerHTML = dataFetch[i].timeframes.monthly.previous + 'hrs'
+    switch (dataFetch[i].title) {
+      case 'Work':
+        updateCurrent(currentWork, dataFetch, i, 'monthly')
+        updatePrevious(prevWork, dataFetch, i, 'monthly')
+        break
+      case 'Play':
+        updateCurrent(currentPlay, dataFetch, i, 'monthly')
+        updatePrevious(prevPlay, dataFetch, i, 'monthly')
+        break
+      case 'Study':
+        updateCurrent(currentStudy, dataFetch, i, 'monthly')
+        updatePrevious(prevStudy, dataFetch, i, 'monthly')
+        break
+      case 'Exercise':
+        updateCurrent(currentExercise, dataFetch, i, 'monthly')
+        updatePrevious(prevExercise, dataFetch, i, 'monthly')
+        break
+      case 'Social':
+        updateCurrent(currentSocial, dataFetch, i, 'monthly')
+        updatePrevious(prevSocial, dataFetch, i, 'monthly')
+        break
+      default:
+        updateCurrent(currentCare, dataFetch, i, 'monthly')
+        updatePrevious(prevCare, dataFetch, i, 'monthly')
     }
   }
 
-  navDaily.style.color = '#6f76c8'
-  navWeekly.style.color = '#6f76c8'
-  navMonthly.style.color = '#fff'
+  updateButtonColor(navDaily, '#6f76c8')
+  updateButtonColor(navWeekly, '#6f76c8')
+  updateButtonColor(navMonthly, '#fff')
 }
